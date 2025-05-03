acdbd01dfbe5607cec0dd545a98d1e7937d78f91
Mostafa Hussein <158592935+MostafaHusseinAli20@users.noreply.github.com>
2025-05-03 15:39:10 +0300

    Update Achievements.jsx

--- src/components/about/Achievements.jsx
+++ src/components/about/Achievements.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 const achievementsContent = [
-  { title: "4", subTitle1: "years of", subTitle2: "experience" },
-  { title: "40", subTitle1: "completed", subTitle2: "projects" },
-  { title: "28  ", subTitle1: "Happy", subTitle2: "customers" },
-  { title: "20", subTitle1: "awards", subTitle2: "won" },
+  { title: "2", subTitle1: "years of", subTitle2: "experience" },
+  { title: "30", subTitle1: "completed", subTitle2: "projects" },
+  { title: "20  ", subTitle1: "Happy", subTitle2: "customers" },
+  { title: "15", subTitle1: "awards", subTitle2: "won" },
 ];
 
 const Achievements = () => {
